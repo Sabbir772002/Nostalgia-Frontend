7072d600eec857ee9b92cd6ea2d00049d1923758
sabbir772002 <sabbir772002@gmail.com>
2024-10-06 11:39:46 +0600

    S

--- src/util/api.js
+++
@@ -1,9 +0,0 @@
-const api = {
-   url:"http://localhost"
-   // url:"http://192.168.0.102"
-  // url:"http://192.168.1.105"
-  // url:"http://192.168.1.105"
-   // url:"http://10.10.200.137"
-  };
-  export default api;
-  
